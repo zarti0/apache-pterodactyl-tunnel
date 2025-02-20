808e838b01b8504d40397af7abc6fcbf52bd9b25
Zarti 0 <killerriad47@gmail.com>
2025-02-20 15:30:31 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sleep 1
+
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
+
+source /home/container/apache2/envvars
+apache2 -f apache2.conf -d /home/container/apache2
